64593c41604015b11a4a1962b2406c7ecdbb1aba
小倉裕一 <j211025ogu@gmail.com>
2019-03-04 17:03:57 +0900

    Add example

---
+++ _example/alert_odd/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/y-ogura/queue"
+)
+
+var (
+	maxWorkers   = flag.Int("max_workers", 5, "The number of workers to start")
+	maxQueueSize = flag.Int("max_queue_size", 10000, "The size of job queue")
+)
+
+func main() {
+	flag.Parse()
+	// create the job queue.
+	jobQueue := make(chan queue.Job, *maxQueueSize)
+
+	// start the dispatcher.
+	dispatcher := queue.NewDispatcher(jobQueue, *maxWorkers)
+	dispatcher.Run()
+
+	for i := 0; i < 100; i++ {
+		num := i
+		// add new job
+		jobQueue <- func() error {
+			err := alertOdd(num)
+			return err
+		}
+	}
+
+	time.Sleep(10 * time.Second)
+}
+
+func alertOdd(num int) error {
+	if num%2 == 0 {
+		return nil
+	}
+	return fmt.Errorf("number %d is odd", num)
+}
